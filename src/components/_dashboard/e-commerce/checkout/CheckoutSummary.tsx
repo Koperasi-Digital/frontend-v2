84ee8b05304c7bc70d6286af15644e967d569c46
Michael Hans <michaelhans777@gmail.com>
2022-01-11 02:55:43 +0700

    remove some field

--- src/components/_dashboard/e-commerce/checkout/CheckoutSummary.tsx
+++ src/components/_dashboard/e-commerce/checkout/CheckoutSummary.tsx
@@ -68,12 +68,12 @@ export default function CheckoutSummary({
             <Typography variant="subtitle2">{fCurrency(subtotal)}</Typography>
           </Stack>
 
-          <Stack direction="row" justifyContent="space-between">
+          {/* <Stack direction="row" justifyContent="space-between">
             <Typography variant="body2" sx={{ color: 'text.secondary' }}>
               Discount
             </Typography>
             <Typography variant="subtitle2">{discount ? fCurrency(-discount) : '-'}</Typography>
-          </Stack>
+          </Stack> */}
 
           <Stack direction="row" justifyContent="space-between">
             <Typography variant="body2" sx={{ color: 'text.secondary' }}>
@@ -98,7 +98,7 @@ export default function CheckoutSummary({
             </Box>
           </Stack>
 
-          {enableDiscount && onApplyDiscount && (
+          {/* {enableDiscount && onApplyDiscount && (
             <TextField
               fullWidth
               placeholder="Discount codes / Gifts"
@@ -113,7 +113,7 @@ export default function CheckoutSummary({
                 )
               }}
             />
-          )}
+          )} */}
         </Stack>
       </CardContent>
     </Card>
