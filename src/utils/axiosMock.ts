46744d56bbed8c58dab71df3c644a460f139141d
tonyeko <tony.yuwono@gmail.com>
2022-02-05 23:08:42 +0700

    split `axios` for mock and real request

--- src/utils/axiosMock.ts
+++ src/utils/axiosMock.ts
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 // ----------------------------------------------------------------------
 
-const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:4000/v1/'
-});
+const axiosInstance = axios.create();
 
 axiosInstance.interceptors.response.use(
   (response) => response,
