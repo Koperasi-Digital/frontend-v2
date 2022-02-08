301711168d9bd8717d67151584d667dd7c5398da
tonyeko <tony.yuwono@gmail.com>
2022-02-08 13:01:09 +0700

    comment unused sections

--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -1,7 +1,7 @@
 import { Link as RouterLink } from 'react-router-dom';
 // material
 import { styled } from '@mui/material/styles';
-import { Box, Card, Stack, Link, Container, Typography } from '@mui/material';
+import { Box, Stack, Link, Container, Typography } from '@mui/material';
 // routes
 import { PATH_AUTH } from '../../routes/paths';
 // layouts
@@ -19,14 +19,14 @@ const RootStyle = styled(Page)(({ theme }) => ({
   }
 }));
 
-const SectionStyle = styled(Card)(({ theme }) => ({
-  width: '100%',
-  maxWidth: 464,
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  margin: theme.spacing(2, 0, 2, 2)
-}));
+// const SectionStyle = styled(Card)(({ theme }) => ({
+//   width: '100%',
+//   maxWidth: 464,
+//   display: 'flex',
+//   flexDirection: 'column',
+//   justifyContent: 'center',
+//   margin: theme.spacing(2, 0, 2, 2)
+// }));
 
 const ContentStyle = styled('div')(({ theme }) => ({
   maxWidth: 480,
@@ -50,14 +50,14 @@ export default function Login() {
         </Link>
       </AuthLayout>
 
-      <MHidden width="mdDown">
+      {/* <MHidden width="mdDown">
         <SectionStyle>
           <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
             Hi, Welcome Back
           </Typography>
           <img src="/static/illustrations/illustration_login.png" alt="login" />
         </SectionStyle>
-      </MHidden>
+      </MHidden> */}
 
       <Container maxWidth="sm">
         <ContentStyle>
