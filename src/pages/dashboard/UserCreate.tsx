44257cab32819e00887da93299ed76ff38aaf8bc
Tony Eko Yuwono <tony.yuwono@traveloka.com>
2022-04-14 00:42:50 +0700

    add addressBook in user detail/edit for admin

--- src/pages/dashboard/UserCreate.tsx
+++ src/pages/dashboard/UserCreate.tsx
@@ -1,17 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { paramCase } from 'change-case';
 import { useParams, useLocation } from 'react-router-dom';
 // material
-import { Container } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 // redux
 import { useDispatch, useSelector, RootState } from '../../redux/store';
 import { getUserList } from '../../redux/slices/user';
 // routes
 import { PATH_DASHBOARD } from '../../routes/paths';
+// utils
+import axios from 'utils/axios';
 // components
 import Page from '../../components/Page';
 import HeaderBreadcrumbs from '../../components/HeaderBreadcrumbs';
 import UserNewForm from '../../components/_dashboard/user/UserNewForm';
+import { AccountAddressBook } from 'components/_dashboard/user/account';
 
 // ----------------------------------------------------------------------
 
@@ -22,10 +25,23 @@ export default function UserCreate() {
   const { userList } = useSelector((state: RootState) => state.user);
   const isEdit = pathname.includes('edit');
   const currentUser = userList.find((user) => paramCase(user.displayName) === name);
+  const [addressBook, setAddressBook] = useState([]);
+
+  const getUserAddressBook = (userId: string) => {
+    axios.get(`user-addresses`, { params: { userId } }).then((response) => {
+      setAddressBook(response.data.payload);
+    });
+  };
 
   useEffect(() => {
     dispatch(getUserList());
   }, [dispatch]);
+
+  useEffect(() => {
+    if (currentUser) {
+      getUserAddressBook(currentUser.id);
+    }
+  }, [currentUser]);
 
   return (
     <Page
@@ -43,7 +59,14 @@ export default function UserCreate() {
           ]}
         />
 
-        <UserNewForm isEdit={isEdit} currentUser={currentUser} />
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <UserNewForm isEdit={isEdit} currentUser={currentUser} />
+          </Grid>
+          <Grid item xs={12}>
+            <AccountAddressBook addressBook={addressBook} isEdit={true} />
+          </Grid>
+        </Grid>
       </Container>
     </Page>
   );
