6d7b74ae94dbc1502bf0b74ebed68701172034b4
rifaldyaristya23 <13518082@std.stei.itb.ac.id>
2022-05-12 17:51:11 +0700

    add list function for coop transaction

--- src/utils/financeAxios/financeCoopTransaction.ts
+++ src/utils/financeAxios/financeCoopTransaction.ts
@@ -7,7 +7,7 @@ export async function handleCreateCoopTransaction(param: {
   status: string;
 }) {
   try {
-    const response = await axios.post('coop-transaction/create', param);
+    const response = await axios.post('coop-transaction', param);
     return response.data.payload;
   } catch (e) {
     console.log(e);
@@ -15,18 +15,28 @@ export async function handleCreateCoopTransaction(param: {
   }
 }
 
-export async function handleShowCoopTransaction(
-  destUserId: number,
-  fromDate: string,
-  toDate: string
-) {
+export async function handleListCoopTransactions(fromDate: string, toDate: string) {
   try {
-    const response = await axios.get(
-      'coop-transaction/show/' + destUserId + '/' + fromDate + '/' + toDate
-    );
+    const response = await axios.get(`coop-transaction/${fromDate}/${toDate}`);
     return response.data.payload;
   } catch (e) {
     console.log(e);
     return null;
   }
 }
+
+// export async function handleShowCoopTransaction(
+//   destUserId: number,
+//   fromDate: string,
+//   toDate: string
+// ) {
+//   try {
+//     const response = await axios.get(
+//       'coop-transaction/show/' + destUserId + '/' + fromDate + '/' + toDate
+//     );
+//     return response.data.payload;
+//   } catch (e) {
+//     console.log(e);
+//     return null;
+//   }
+// }
