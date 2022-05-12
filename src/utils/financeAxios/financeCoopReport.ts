b058187416a452ff9a5604828716973097458e13
rifaldyaristya23 <13518082@std.stei.itb.ac.id>
2022-05-12 17:39:50 +0700

    coop finance report annual fetch function

--- src/utils/financeAxios/financeCoopReport.ts
+++ src/utils/financeAxios/financeCoopReport.ts
@@ -10,6 +10,16 @@ export async function handleGetCoopNeracaInfo(periode: string) {
   }
 }
 
+export async function handleGetCoopNeracaAnnualData(year: number){
+  try{
+    const response = await axios.get(`coop-laporan-neraca/get-annual-data/${year}`);
+    return response.data.payload;
+  } catch(e){
+    console.log(e);
+    return undefined;
+  }
+}
+
 export async function handleGetCoopLabaRugiInfo(periode: string) {
   try {
     const response = await axios.get('coop-laporan-laba-rugi/' + periode);
@@ -20,11 +30,31 @@ export async function handleGetCoopLabaRugiInfo(periode: string) {
   }
 }
 
+export async function handleGetCoopLabaRugiAnnualData(year: number){
+  try{
+    const response = await axios.get(`coop-laporan-laba-rugi/get-annual-data/${year}`);
+    return response.data.payload;
+  } catch(e){
+    console.log(e);
+    return undefined;
+  }
+}
+
 export async function handleGetCoopArusKasInfo(periode: string) {
   try {
     const response = await axios.get('coop-laporan-arus-kas/' + periode);
     return response.data.payload;
   } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+}
+
+export async function handleGetCoopArusKasAnnualData(year: number){
+  try{
+    const response = await axios.get(`coop-laporan-arus-kas/get-annual-data/${year}`);
+    return response.data.payload;
+  } catch(e){
     console.log(e);
     return undefined;
   }
