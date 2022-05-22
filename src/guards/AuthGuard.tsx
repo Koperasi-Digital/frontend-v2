acc7af292c9bb47a18f4f20c9c9ef174c02dba4e
Tony Eko Yuwono <tony.yuwono@traveloka.com>
2022-05-22 17:25:07 +0700

    redirect to `home` page after login

--- src/guards/AuthGuard.tsx
+++ src/guards/AuthGuard.tsx
@@ -1,9 +1,8 @@
-import { useState, ReactNode } from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
+import { ReactNode } from 'react';
+import { Navigate } from 'react-router-dom';
+import { PATH_AUTH } from 'routes/paths';
 // hooks
 import useAuth from '../hooks/useAuth';
-// pages
-import Login from '../pages/authentication/Login';
 
 // ----------------------------------------------------------------------
 
@@ -13,19 +12,9 @@ type AuthGuardProps = {
 
 export default function AuthGuard({ children }: AuthGuardProps) {
   const { isAuthenticated } = useAuth();
-  const { pathname } = useLocation();
-  const [requestedLocation, setRequestedLocation] = useState<string | null>(null);
 
   if (!isAuthenticated) {
-    if (pathname !== requestedLocation) {
-      setRequestedLocation(pathname);
-    }
-    return <Login />;
-  }
-
-  if (requestedLocation && pathname !== requestedLocation) {
-    setRequestedLocation(null);
-    return <Navigate to={requestedLocation} />;
+    return <Navigate to={PATH_AUTH.login} />;
   }
 
   return <>{children}</>;
