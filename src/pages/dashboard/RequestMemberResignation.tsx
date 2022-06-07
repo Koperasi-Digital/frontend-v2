4b4da4c4c1af7aabfd7a5e02b2bffe6f3b6503ae
rifaldyaristya23 <13518082@std.stei.itb.ac.id>
2022-06-07 23:05:42 +0700

    handle bank account not exist @member resign req

--- src/pages/dashboard/RequestMemberResignation.tsx
+++ src/pages/dashboard/RequestMemberResignation.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 // material
 import { styled } from '@mui/material/styles';
-import { Alert, AlertTitle, Box, Container, Typography } from '@mui/material';
+import { Alert, AlertTitle, Box, Container, Typography, Link, Button, Stack } from '@mui/material';
 // utils
 import axios from 'utils/axios';
 import { LoadingButton } from '@mui/lab';
@@ -10,6 +10,12 @@ import useAuth from 'hooks/useAuth';
 // components
 import Page from 'components/Page';
 import { RequestMemberResignationForm } from 'components/_dashboard/user/member-resignation';
+
+import { handleGetBankAccount } from 'utils/financeAxios/financeBankAccount';
+import { Link as RouterLink } from 'react-router-dom';
+
+// routes
+import { PATH_DASHBOARD } from 'routes/paths';
 
 // ----------------------------------------------------------------------
 
@@ -22,6 +28,12 @@ const ContentStyle = styled('div')(({ theme }) => ({
   padding: theme.spacing(12, 0)
 }));
 
+type BankAccount = {
+  accountNumber: string;
+  accountName: string;
+  bankName: string;
+};
+
 // ----------------------------------------------------------------------
 
 export default function RequestMemberResignation() {
@@ -29,6 +41,8 @@ export default function RequestMemberResignation() {
   const [isMember, setIsMember] = useState(false);
   const [memberResignation, setMemberResignation] = useState<any>();
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [bankAccount, setBankAccount] = useState<BankAccount>();
+  const linkTo = PATH_DASHBOARD.finance.disbursementRequest;
 
   // const getUserMemberResignation = (userId: number) => {
   //   axios
@@ -46,6 +60,16 @@ export default function RequestMemberResignation() {
   // }, [user]);
 
   useEffect(() => {
+    const fetchData = async () => {
+      const bankAccount = await handleGetBankAccount();
+      if (bankAccount) {
+        setBankAccount(bankAccount);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
     if (user && user.roles) {
       setIsMember(user.roles.map((role: any) => role.name).includes('MEMBER'));
     }
@@ -54,45 +78,86 @@ export default function RequestMemberResignation() {
   const renderContent = () => {
     if (isMember) {
       return (
-        <ContentStyle>
-          {memberResignation ? (
-            <Box sx={{ mb: 5, display: 'flex', alignItems: 'center' }}>
-              <Box sx={{ flexGrow: 1 }}>
-                <Typography variant="h4" gutterBottom>
-                  Pengunduran Keanggotaan Koperasi
-                </Typography>
-                <Typography sx={{ color: 'text.secondary' }}>
-                  Request pengunduran diri dari keanggotaan koperasi telah dikirimkan kepada Admin
-                </Typography>
-                <LoadingButton
-                  fullWidth
-                  size="large"
-                  type="submit"
-                  variant="contained"
-                  loading={isSubmitting}
-                  sx={{ my: 3 }}
-                >
-                  Batalkan
-                </LoadingButton>
-              </Box>
-            </Box>
-          ) : (
-            <>
-              <Box sx={{ mb: 5, display: 'flex', alignItems: 'center' }}>
-                <Box sx={{ flexGrow: 1 }}>
-                  <Typography variant="h4" gutterBottom>
-                    Pengunduran Keanggotaan Koperasi
-                  </Typography>
-                  <Typography sx={{ color: 'text.secondary' }}>
-                    Silakan isi formulir berikut untuk request pengunduran diri dari keanggotaan
-                    koperasi.
-                  </Typography>
+        <>
+          {bankAccount ? (
+            <ContentStyle>
+              {memberResignation ? (
+                <Box sx={{ mb: 5, display: 'flex', alignItems: 'center' }}>
+                  <Box sx={{ flexGrow: 1 }}>
+                    <Typography variant="h4" gutterBottom>
+                      Pengunduran Keanggotaan Koperasi
+                    </Typography>
+                    <Typography sx={{ color: 'text.secondary' }}>
+                      Request pengunduran diri dari keanggotaan koperasi telah dikirimkan kepada
+                      Admin
+                    </Typography>
+                    <LoadingButton
+                      fullWidth
+                      size="large"
+                      type="submit"
+                      variant="contained"
+                      loading={isSubmitting}
+                      sx={{ my: 3 }}
+                    >
+                      Batalkan
+                    </LoadingButton>
+                  </Box>
                 </Box>
-              </Box>
-              <RequestMemberResignationForm />
-            </>
+              ) : (
+                <>
+                  <Box sx={{ mb: 5, display: 'flex', alignItems: 'center' }}>
+                    <Box sx={{ flexGrow: 1 }}>
+                      <Typography variant="h4" gutterBottom>
+                        Pengunduran Keanggotaan Koperasi
+                      </Typography>
+                      <Typography sx={{ color: 'text.secondary' }}>
+                        Silakan isi formulir berikut untuk request pengunduran diri dari keanggotaan
+                        koperasi.
+                      </Typography>
+                    </Box>
+                  </Box>
+                  <RequestMemberResignationForm />
+                </>
+              )}
+            </ContentStyle>
+          ) : (
+            <Container
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%'
+              }}
+            >
+              <Alert severity="error">
+                <AlertTitle>Akses ditolak</AlertTitle>
+                <Stack
+                  spacing={1}
+                  direction="column"
+                  alignItems="center"
+                  justifyContent="space-between"
+                >
+                  <Typography>
+                    Anda tidak memiliki akun bank yang terdaftar untuk pengembalian keuangan.
+                    Silakan daftarkan akun bank terlebih dahulu
+                  </Typography>
+                  <Link
+                    to={linkTo}
+                    color="inherit"
+                    component={RouterLink}
+                    style={{
+                      textDecoration: 'none'
+                    }}
+                  >
+                    <Button size="medium" variant="contained">
+                      Daftarkan akun bank
+                    </Button>
+                  </Link>
+                </Stack>
+              </Alert>
+            </Container>
           )}
-        </ContentStyle>
+        </>
       );
     }
     return (
