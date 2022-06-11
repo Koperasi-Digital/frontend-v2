30313bf73bffc1eab1bf0a89a10a71cd8f6a3754
rifaldyaristya23 <13518082@std.stei.itb.ac.id>
2022-06-12 01:44:19 +0700

    revert shipment try to handle rajaongkir error

--- src/utils/checkoutAxios/shipment.ts
+++ src/utils/checkoutAxios/shipment.ts
@@ -4,65 +4,13 @@ import axios from '../axios';
 // Get all shipment fee
 export async function getAllShipmentCost(shipmentInfo: ShipmentForm) {
   try {
-    // const SHIPMENT_TYPE = ['tiki', 'pos', 'jne'];
-    // let allShipment = [];
-    // for (const SHIPMENT of SHIPMENT_TYPE) {
-    //   const response = await axios.post('shipment/cost', { ...shipmentInfo, courier: SHIPMENT });
-    //   allShipment.push(response.data.payload);
-    // }
-    // return allShipment;
-    return [
-      {
-        code: 'jne',
-        name: 'Jalur Nugraha Ekakurir (JNE)',
-        costs: [
-          {
-            service: 'OKE',
-            description: 'Ongkos Kirim Ekonomis',
-            cost: [
-              {
-                value: 38000,
-                etd: '4-5',
-                note: ''
-              }
-            ]
-          },
-          {
-            service: 'REG',
-            description: 'Layanan Reguler',
-            cost: [
-              {
-                value: 44000,
-                etd: '2-3',
-                note: ''
-              }
-            ]
-          },
-          {
-            service: 'SPS',
-            description: 'Super Speed',
-            cost: [
-              {
-                value: 349000,
-                etd: '',
-                note: ''
-              }
-            ]
-          },
-          {
-            service: 'YES',
-            description: 'Yakin Esok Sampai',
-            cost: [
-              {
-                value: 98000,
-                etd: '1-1',
-                note: ''
-              }
-            ]
-          }
-        ]
-      }
-    ];
+    const SHIPMENT_TYPE = ['tiki', 'pos', 'jne'];
+    let allShipment = [];
+    for (const SHIPMENT of SHIPMENT_TYPE) {
+      const response = await axios.post('shipment/cost', { ...shipmentInfo, courier: SHIPMENT });
+      allShipment.push(response.data.payload);
+    }
+    return allShipment;
   } catch (e) {
     console.log(e);
     return [];
